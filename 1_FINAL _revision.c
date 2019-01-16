7ffed77b45e726edc7fb9b51a5ffd9f5febc6696
2000Mahan <46421998+2000Mahan@users.noreply.github.com>
2019-01-16 21:52:58 +0330

    Creating Linked List

---
+++ 1_FINAL _revision.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//CREATING LINKED LIST//
+struct node{
+char issue[200];
+char decision1[200];
+int people_decision1;
+int darbar_decision1;
+int khazaneh_decision1;
+char decision2[200];
+int people_decision2;
+int darbar_decision2;
+int khazaneh_decision2;
+struct node* next;
+};
+struct node *create_node(char issue[200], char decision1[200], int people_decision1, int darbar_decision1, int khazaneh_decision1,char decision2[200], int people_decision2, int darbar_decision2, int khazaneh_decision2)
+{
+struct node *nn;
+nn=(struct node*)malloc(sizeof(struct node));
+strcpy(issue, nn->issue);
+strcpy(decision1, nn->decision1);
+nn->people_decision1=people_decision1;
+nn->darbar_decision1=darbar_decision1;
+nn->khazaneh_decision1=khazaneh_decision1;
+strcpy(decision2, nn->decision2);
+nn->people_decision2=people_decision2;
+nn->darbar_decision2=darbar_decision2;
+nn->khazaneh_decision2=khazaneh_decision2;
+nn->next=NULL;
+
+return nn;
+
+
+}
+
+int main()
+{
+
+
+
+}
